f2c309c6159a779cb333d417da64618a6e2e458b
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2025-02-25 19:45:51 +0200

    I asked claude to give me code I pasted the existing python scripts and tests to see what it does

---
+++ bin/jekyll_post.rb
@@ -0,0 +1,153 @@
+#!/usr/bin/env ruby
+
+require 'date'
+require 'time'
+require 'optparse'
+require 'fileutils'
+
+DEFAULT_EXT = 'md'
+
+class JekyllPost
+  def self.format_date_for_file(date)
+    year = date.year.to_s
+    month = date.month.to_s.rjust(2, '0')
+    day = date.day.to_s.rjust(2, '0')
+    "#{year}-#{month}-#{day}"
+  end
+
+  def self.today_or_parse_input(input)
+    if input && !input.empty?
+      Date.parse(input).to_datetime
+    else
+      DateTime.now
+    end
+  end
+
+  def self.get_z_delta(date)
+    offset = date.offset
+    if offset == 0
+      '+00:00'
+    else
+      hours = (offset * 24).to_i
+      minutes = ((offset * 24 * 60) % 60).to_i
+      format('%+03d:%02d', hours, minutes)
+    end
+  end
+
+  def self.format_for_date_field(date)
+    "#{date.strftime('%Y-%m-%dT%H:%M:%S')}#{date.strftime('%:z')}"
+  end
+
+  def self.yaml_file(title, date, tags)
+    content = []
+    content << "---\n"
+    content << "layout: post\n"
+    content << "title: #{title}\n"
+    content << "date: #{format_for_date_field(date)}\n"
+
+    if tags && !tags.empty?
+      content << "categories:\n"
+      tags.each do |tag|
+        content << "- #{tag}\n"
+      end
+    end
+    
+    content << "---\n\n\n"
+    content.join
+  end
+
+  def self.format_title(title)
+    dash_title = title.gsub(' ', '-')
+    
+    # clean up any special characters from the dash-title
+    temp_title = ''
+    dash_title.each_char do |char|
+      if char =~ /[a-zA-Z0-9]/ || char == '-'
+        temp_title += char
+      end
+    end
+    
+    temp_title
+  end
+
+  def self.main
+    options = {}
+    
+    opt_parser = OptionParser.new do |opts|
+      opts.banner = "Usage: jekyll-post [options] title"
+
+      opts.on("-D", "--Dir DIRECTORY", "Specify the directory for the post file") do |dir|
+        options[:dir] = dir
+      end
+
+      opts.on("-d", "--date DATE", "Specify the post date (YYYY-MM-DD)") do |date|
+        options[:date] = date
+      end
+
+      opts.on("-n", "--name NAME", "Specify the name of the post file") do |name|
+        options[:name] = name
+      end
+
+      opts.on("-t", "--tags TAGS", "Specify tags for the post") do |tags|
+        options[:tags] = tags
+      end
+
+      opts.on("-w", "--write", "Open the file for writing after creation") do
+        options[:write] = true
+      end
+    end
+
+    opt_parser.parse!
+    
+    if ARGV.empty?
+      puts "Error: Title is required"
+      puts opt_parser
+      exit(1)
+    end
+
+    title = ARGV[0]
+    
+    dash_title = if options[:name]
+                  format_title(options[:name])
+                else
+                  format_title(title)
+                end
+
+    date = today_or_parse_input(options[:date])
+    
+    filename = "#{format_date_for_file(date)}-#{dash_title}.#{DEFAULT_EXT}"
+    
+    if options[:dir]
+      filename = File.join(options[:dir], filename)
+    else
+      filename = File.join(File.dirname(File.expand_path(__FILE__)), '..', '_posts', filename)
+    end
+    
+    if File.exist?(filename)
+      puts "#{filename} already exists."
+      puts "Jekyll post NOT created."
+      exit(1)
+    end
+    
+    tags = options[:tags] ? options[:tags].split(' ') : []
+    
+    begin
+      File.open(filename, 'w') do |f|
+        f.write(yaml_file(title, date, tags))
+      end
+    rescue IOError
+      puts "Issue writing to Jekyll post file."
+      exit(1)
+    end
+    
+    if options[:write]
+      system('vim', filename)
+    end
+    
+    puts "New Jekyll post \"#{title}\" has been created -- #{filename}"
+  end
+end
+
+if __FILE__ == $0
+  JekyllPost.main
+end
---
+++ bin/jekyll_post_tests.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/env ruby
+
+require 'rspec'
+require 'date'
+require 'time'
+require_relative 'jekyll_post'
+
+# Custom timezone class to replicate Python's TIMEZONE
+class CustomTimezone < TZInfo::Timezone
+  def utc_offset
+    3600  # +1 hour in seconds
+  end
+  
+  def dst?
+    false
+  end
+end
+
+RSpec.describe JekyllPost do
+  before do
+    # Create a timezone object
+    @timezone = CustomTimezone.new
+    # Create a datetime object with the custom timezone
+    @date = DateTime.new(2014, 4, 9, 12, 30, 0, '+01:00')
+  end
+
+  describe '#format_date_for_file' do
+    it 'formats the date correctly for file names' do
+      expect(JekyllPost.format_date_for_file(@date)).to eq('2014-04-09')
+    end
+  end
+
+  describe '#format_for_date_field' do
+    it 'formats the date correctly for YAML front matter' do
+      expect(JekyllPost.format_for_date_field(@date)).to eq('2014-04-09T12:30:00+01:00')
+    end
+  end
+
+  describe '#yaml_file' do
+    it 'generates the correct YAML front matter' do
+      expected = <<~YAML
+        ---
+        layout: post
+        title: A pretty title
+        date: 2014-04-09T12:30:00+01:00
+        ---
+
+
+      YAML
+      
+      actual = JekyllPost.yaml_file("A pretty title", @date, nil)
+      expect(actual).to eq(expected)
+    end
+  end
+  
+  # Commented out as in the original Python code
+  # describe '#today_or_parse_input' do
+  #   it 'returns today\'s date when empty string is provided' do
+  #     today = JekyllPost.today_or_parse_input('')
+  #     formatted = JekyllPost.format_for_date_field(today)
+  #     expect(formatted).to eq('2014-04-09T12:30:00+01:00')
+  #   end
+  # end
+end
